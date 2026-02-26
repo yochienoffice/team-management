4076137286c5ed580f983d9e378fda9606182343
Yoyo <jchien@hotsaucepos.com>
2026-02-26 13:13:01 +0800

    Add register form and related components

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,12 +3,12 @@
 import { LoginForm } from "@/components/LoginForm";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { RegisterForm } from "./RegisterForm";
 
 export default function LoginPage() {
   return (
     <div className="flex flex-col items-center justify-center w-full h-[100dvh] bg-hs-main-bg">
-      <LoginForm />
-      <Button><Link href="/register">註冊會員</Link></Button>
+      <RegisterForm />
     </div>
   );
 }
